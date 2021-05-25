edacbef520689ddcc992891ca696a387d86b38e2
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2021-05-25 16:35:04 -0300

    Inicial setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import {GlobalStyle} from "./GlobalStyle"
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import useState from "react"
+import useContext from "react"
 
 export default function App(){
-
     return (
         <>
             <GlobalStyle/>
-            <h1>Hello world</h1>
+            <Login/>
+            <Registration/>
+
         </>
-    ) 
+    )
 }
